46a80f6e68438ff9cdff6a18c7d0459d2134d1c1
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2024-04-02 21:17:52 -0400

    starter folder strcuture for flask

---
+++ static/script.js
@@ -0,0 +1,9 @@
+// This is a basic JavaScript function to display a message in the console.
+function showMessage() {
+    console.log("Hello from script.js!");
+}
+
+// Call the showMessage function when the page is loaded.
+window.onload = function() {
+    showMessage();
+};
